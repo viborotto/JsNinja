5336cdb2a7324f3405265226e81d99c951bd5b1c
viborotto <vittoriaborotto@gmail.com>
2020-01-11 15:29:38 -0300

    Criaçao de objeto final

--- challenge-12.js
+++ challenge-12.js
@@ -25,3 +25,11 @@ var newObj = new Object();
 var obj = Object.create();
 Object.prototype()//é um prototipo do objeto que esta sendo criado  
 //cada vez que vc cria um obj ele herda seu prototipo
+//cada objeto tem um prototipo herdado de outro objeto
+var obj = {x:1, y:2};
+var obj2 = Object.create(obj);//criado a partir do outro    
+obj2 //retorna vazio
+obj2.prototype //retorna undefined
+obj2.x //retorna 1 pq herdou as propriedades do obj
+obj.hasOwnProperty('x')
+//retorna true
